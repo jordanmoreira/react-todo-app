e95892101f14f3dd16f01ea7d21f6a1e9bd05f95
Moreira <Jordan.Moreira@sovos.com>
2019-01-22 13:16:38 -0200

    todo refactoring done, change events

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -6,7 +6,9 @@ export default props => (
     <div role='form' className='todoForm'>
         <Grid cols='12 9 10'>
             <input id='description' className='form-control'
-                placeholder='Add a task' value={props.descriotion}></input>
+                placeholder='Add a task'
+                onChange={props.handleChange}
+                value={props.description}></input>
         </Grid>
 
         <Grid cols='12 3 2'>
