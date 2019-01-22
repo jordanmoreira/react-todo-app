62a192fac480113d269bba687c4c8141f3c6eeb9
Moreira <Jordan.Moreira@sovos.com>
2019-01-22 10:26:44 -0200

    routes

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import PageHeader from '../template/pageHeader'
 
 export default props => (
     <div>
-        <h1>About</h1>
+        <PageHeader name='About' small='Us'></PageHeader>
+        <h2>Lorem ipsum</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Suspendisse eget justo vitae felis malesuada malesuada et non ante.
+            Proin fringilla sem sit amet est molestie, eget scelerisque velit fringilla.
+            Etiam tempor auctor purus commodo varius. In id augue quis augue tristique rutrum.
+            Cras mollis luctus quam ac auctor. Aliquam semper venenatis neque, a tristique velit eleifend non.
+            Nunc libero tortor, fermentum ac fermentum id, congue eget mi. In vehicula interdum luctus.
+            Fusce quis mi nec sapien lobortis eleifend imperdiet non turpis. In mollis tempus ornare.
+            Cras in eros magna. Cras et lectus in felis interdum vulputate. Cras at elit faucibus,
+            porttitor ligula in, dictum velit. Pellentesque pharetra dolor at augue pulvinar,
+            a ultrices erat placerat.</p>
     </div>
 )
