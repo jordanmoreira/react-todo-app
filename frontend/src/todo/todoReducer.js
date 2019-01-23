eddd1a628b3536e1d9ee0fdad150275d796a7f49
Moreira <Jordan.Moreira@sovos.com>
2019-01-23 14:49:05 -0200

    todo add and redux-promise middleware

--- frontend/src/todo/todoReducer.js
+++ frontend/src/todo/todoReducer.js
@@ -1,24 +1,13 @@
-const INITIAL_STATE = {
-    description: 'Jogar MK',
-    list: [{
-        _id: 1,
-        description: 'Pagar fatura',
-        done: true
-    }, {
-        _id: 2,
-        description: 'Assistir',
-        done: false
-    }, {
-        _id: 3,
-        description: 'Jogar',
-        done: false
-    }]
-}
+const INITIAL_STATE = { description: '', list: [] }
 
 export default (state = INITIAL_STATE, action) => {
     switch (action.type) {
         case 'DESCRIPTION_CHANGED':
             return { ...state, description: action.payload }
+        case 'TODO_SEARCHED':
+            return { ...state, list: action.payload.data }
+        case 'TODO_ADDED':
+            return { ...state, description: '' }
         default:
             return state
     }
